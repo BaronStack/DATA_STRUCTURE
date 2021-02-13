d2cddaf58cb28271f103ef5091b10b59ed160fcb
BaronStack <2689496754@qq.com>
2021-02-13 17:43:11 +0800

    [other] add greedy alg: distribute candies, get the smallest coin nums

---
+++ greedy/coins_charge.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int cmp(pair<int, int> a, pair<int, int> b) {
+  return a.first > b.first;
+}
+
+void getCoinNums(vector<pair<int,int>> coins,
+                int K, vector<pair<int,int>> &result) {
+  if (coins.size() == 0) {
+    return;
+  }
+
+  int i, tmp;
+  
+  // sort from biger to smaller
+  sort(coins.begin(), coins.end(), cmp);
+
+  i = 0;
+  tmp = 0;
+  while(K && i < coins.size()) {
+    tmp = K / coins[i].first;
+
+    if (tmp != 0) {
+      int real_nums;
+      // defend the real coin nums overhead
+      if (tmp <= coins[i].second) {
+        real_nums = tmp;
+      } else {
+        real_nums = coins[i].second;
+      }
+
+      K -= real_nums * coins[i].first;
+
+      result.push_back(make_pair(coins[i].first, real_nums));
+    }
+    i++;
+  }
+}
+
+int main() {
+  vector<pair<int,int>> coins = {
+    {100,1}, 
+    {50,3}, 
+    {20,1}, 
+    {10, 10}, 
+    {5, 5},
+    {2,2},
+    {1,10}};
+
+  vector <pair<int,int>> result;
+  int K = 193;
+  
+  getCoinNums(coins, K, result);
+
+  int res_nums = 0;
+  for (int i = 0;i < result.size(); i++) {
+    cout << "coin: " << result[i].first << " use "
+                     << result[i].second << endl;
+    res_nums += result[i].second;
+  }
+  cout << "Total coins is : " << res_nums << endl;
+
+  return 0;
+}
---
+++ greedy/distribute_candies.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// simple greedy algorithm: distribute candies
+// six childs' request: 1 1 3 2 2 8
+// five candies size:   4 2 5 7 9
+//
+// Greedy algorithm is suitable to solve the problem.
+// We can let the smaller candy's size to satisfy the 
+// smaller resquest of child.
+int distributeCandy(vector<int> candies,
+    vector<int> childs) {
+  if(candies.size() == 0 || childs.size() == 0) {
+    return 0;
+  }
+
+  // sort, we can compare from small to big request
+  sort(candies.begin(), candies.end());
+  sort(childs.begin(), childs.end());
+
+  int res = 0;
+  int i, j;
+  
+  for (i = 0, j = 0;i < childs.size() && 
+      j < candies.size(); i++,j++) {
+    if (childs[i] <= candies[j]) {
+      res ++;
+    }
+  }
+
+  return res;
+}
+
+int main() {
+  vector<int> candies = {4,2,5,7,9};
+  vector<int> childs = {1,1,3,2,2,8};
+
+  cout << "satisfied childs' num is " 
+       << distributeCandy(candies, childs) 
+       << endl;
+  return 0;
+}
