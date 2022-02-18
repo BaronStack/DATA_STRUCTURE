980d44403737b3a9908fcc2b3f072bbc0b3bba89
BaronStack <2689496754@qq.com>
2022-02-19 00:27:29 +0800

    [add] linklist code

---
+++ linklist/lt-25-merge_two_sorted_linklist.cc
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Method1
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode pre_node(0);
+        ListNode* new_node = &pre_node;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                new_node->next = l1;
+                l1 = l1->next;
+            } else {
+                new_node->next = l2;
+                l2 = l2->next;
+            }
+            new_node = new_node->next;
+        }
+        if (l1) {
+            new_node->next = l1;
+        }
+        if (l2) {
+            new_node->next = l2;
+        }
+
+        return pre_node.next;
+    }
+
+    // Method2 recurrsion
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+
+    // Method3 similar to method1
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode pre_node(0);
+        ListNode* new_node = &pre_node;
+        while (l1 || l2) {
+            if (!l1) {
+                new_node->next = l2;
+                break;
+            }
+            if (!l2) {
+                new_node->next = l1;
+                break;
+            }
+
+            if (l1->val <= l2->val) {
+                new_node->next = l1;
+                l1 = l1->next;
+            } else {
+                new_node->next = l2;
+                l2 = l2->next;
+            }
+            new_node = new_node->next;
+        }
+        return pre_node.next;
+    }
+};
